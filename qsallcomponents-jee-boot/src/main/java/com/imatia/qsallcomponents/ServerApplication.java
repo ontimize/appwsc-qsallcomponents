2144f1241fac280b89c1a80745d992a2644da79a
Enrique Alvarez Pereira <enrique.alvarez@imatia.com>
2021-04-30 14:03:03 +0200

    Removed component scan from ServerApplication

--- qsallcomponents-jee-boot/src/main/java/com/imatia/qsallcomponents/ServerApplication.java
+++ qsallcomponents-jee-boot/src/main/java/com/imatia/qsallcomponents/ServerApplication.java
@@ -3,12 +3,9 @@ package com.imatia.qsallcomponents;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 
 @SpringBootApplication
-//@ComponentScan({ "com.ontimize.jee.server.services.preferences",
-//    "com.ontimize.jee.webclient.remoteconfiguration"})
 public class ServerApplication {
 
 	public static void main(String[] args) {
