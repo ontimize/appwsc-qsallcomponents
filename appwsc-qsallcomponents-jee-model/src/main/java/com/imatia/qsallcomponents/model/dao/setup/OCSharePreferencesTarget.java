e8a1861167332dc429b596cf0e95c19ee7f2888f
Enrique Alvarez Pereira <enrique.alvarez@imatia.com>
2021-05-12 14:09:58 +0200

    Added missing dao

---
+++ appwsc-qsallcomponents-jee-model/src/main/java/com/imatia/qsallcomponents/model/dao/setup/OCSharePreferencesTarget.java
@@ -0,0 +1,22 @@
+package com.imatia.qsallcomponents.model.dao.setup;
+
+import org.springframework.context.annotation.Lazy;
+import org.springframework.stereotype.Repository;
+
+import com.ontimize.jee.server.dao.common.ConfigurationFile;
+import com.ontimize.jee.server.dao.jdbc.OntimizeJdbcDaoSupport;
+
+@Repository(value = "OCSharePreferencesTarget")
+@Lazy
+@ConfigurationFile(configurationFile = "base-dao/setup/OCSharePreferencesTarget.xml", configurationFilePlaceholder = "base-dao/placeholders.properties")
+public class OCSharePreferencesTarget extends OntimizeJdbcDaoSupport {
+
+	public static final String ATTR_ID = "ID_SHARE_TARGET";
+	public static final String ATTR_ID_SHARE = "ID_SHARE";
+	public static final String ATTR_USER_TARGET = "USER_TARGET";
+
+	public OCSharePreferencesTarget() {
+		super();
+	}
+
+}
