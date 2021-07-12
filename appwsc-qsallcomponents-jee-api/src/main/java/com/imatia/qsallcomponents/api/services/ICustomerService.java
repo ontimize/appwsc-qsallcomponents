0f48e677de88af2b3ed204b99bae2c0a1985bb9e
Enrique Alvarez Pereira <enrique.alvarez@imatia.com>
2021-07-12 10:23:24 +0200

    Added dms service

--- appwsc-qsallcomponents-jee-api/src/main/java/com/imatia/qsallcomponents/api/services/ICustomerService.java
+++ appwsc-qsallcomponents-jee-api/src/main/java/com/imatia/qsallcomponents/api/services/ICustomerService.java
@@ -5,31 +5,20 @@ import java.util.Map;
 
 import com.ontimize.jee.common.db.AdvancedEntityResult;
 import com.ontimize.jee.common.dto.EntityResult;
+import com.ontimize.jee.common.exceptions.DmsException;
 import com.ontimize.jee.common.exceptions.OntimizeJEERuntimeException;
 
 public interface ICustomerService {
-
-	/**
-	 * Users query.
-	 *
-	 * @param keysValues
-	 *            the keys values
-	 * @param attributes
-	 *            the attributes
-	 * @return the entity result
-	 * @throws OntimizeJEERuntimeException
-	 *             the ontimize jee exception
-	 */
 
 	// ---- CUSTOMER ----
 
 	public EntityResult customerQuery(Map<?, ?> keysValues, List<?> attributes) throws OntimizeJEERuntimeException;
 
 	// Pagination query
-	public AdvancedEntityResult customerPaginationQuery(Map<?, ?> keysValues, List<?> attributes, int recordNumber, int startIndex, List<?> orderBy)
-			throws OntimizeJEERuntimeException;
+	public AdvancedEntityResult customerPaginationQuery(Map<?, ?> keysValues, List<?> attributes, int recordNumber,
+			int startIndex, List<?> orderBy) throws OntimizeJEERuntimeException;
 
-	public EntityResult customerInsert(Map<?, ?> attributes) throws OntimizeJEERuntimeException;
+	public EntityResult customerInsert(Map<?, ?> attributes) throws OntimizeJEERuntimeException, DmsException;
 
 	public EntityResult customerUpdate(Map<?, ?> attributes, Map<?, ?> KeyValues) throws OntimizeJEERuntimeException;
 
@@ -41,19 +30,23 @@ public interface ICustomerService {
 
 	public EntityResult customerTypeInsert(Map<?, ?> attributes) throws OntimizeJEERuntimeException;
 
-	public EntityResult customerTypeUpdate(Map<?, ?> attributes, Map<?, ?> KeyValues) throws OntimizeJEERuntimeException;
+	public EntityResult customerTypeUpdate(Map<?, ?> attributes, Map<?, ?> KeyValues)
+			throws OntimizeJEERuntimeException;
 
 	public EntityResult customerTypeDelete(Map<?, ?> keyValues) throws OntimizeJEERuntimeException;
 
-	public EntityResult customerTypeAggregateQuery(Map<?, ?> keysValues, List<?> attributes) throws OntimizeJEERuntimeException;
+	public EntityResult customerTypeAggregateQuery(Map<?, ?> keysValues, List<?> attributes)
+			throws OntimizeJEERuntimeException;
 
 	// ---- CUSTOMER ACCOUNT ----
 
-	public EntityResult customerAccountQuery(Map<String, Object> keysValues, List<String> attributes) throws OntimizeJEERuntimeException;
+	public EntityResult customerAccountQuery(Map<String, Object> keysValues, List<String> attributes)
+			throws OntimizeJEERuntimeException;
 
 	public EntityResult customerAccountInsert(Map<String, Object> attributes) throws OntimizeJEERuntimeException;
 
-	public EntityResult customerAccountUpdate(Map<String, Object> attributes, Map<String, Object> KeyValues) throws OntimizeJEERuntimeException;
+	public EntityResult customerAccountUpdate(Map<String, Object> attributes, Map<String, Object> KeyValues)
+			throws OntimizeJEERuntimeException;
 
 	public EntityResult customerAccountDelete(Map<String, Object> keyValues) throws OntimizeJEERuntimeException;
 
