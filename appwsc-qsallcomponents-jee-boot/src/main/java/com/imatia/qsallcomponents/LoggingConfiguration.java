01f98f0a00e9e1bb09e3f7634b0115c4177a6983
Faustino Lage Rego <faustino.lage@gmail.com>
2021-10-08 18:39:50 +0200

    Logging configuration

---
+++ appwsc-qsallcomponents-jee-boot/src/main/java/com/imatia/qsallcomponents/LoggingConfiguration.java
@@ -0,0 +1,16 @@
+package com.imatia.qsallcomponents;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.web.firewall.HttpStatusRequestRejectedHandler;
+import org.springframework.security.web.firewall.RequestRejectedHandler;
+
+@Configuration
+public class LoggingConfiguration {
+
+    @Bean
+    RequestRejectedHandler requestRejectedHandler() {
+        return new HttpStatusRequestRejectedHandler();
+    }
+
+}
