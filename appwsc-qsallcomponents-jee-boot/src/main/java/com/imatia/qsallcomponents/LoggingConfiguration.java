b37b54a1ea46dfe417f1d12fb951cd80ef4cc2d7
Faustino Lage Rego <faustino.lage@gmail.com>
2021-10-08 19:30:12 +0200

    StrictHttpFirewall

--- appwsc-qsallcomponents-jee-boot/src/main/java/com/imatia/qsallcomponents/LoggingConfiguration.java
+++ appwsc-qsallcomponents-jee-boot/src/main/java/com/imatia/qsallcomponents/LoggingConfiguration.java
@@ -2,8 +2,10 @@ package com.imatia.qsallcomponents;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.web.firewall.HttpFirewall;
 import org.springframework.security.web.firewall.HttpStatusRequestRejectedHandler;
 import org.springframework.security.web.firewall.RequestRejectedHandler;
+import org.springframework.security.web.firewall.StrictHttpFirewall;
 
 @Configuration
 public class LoggingConfiguration {
@@ -11,6 +13,13 @@ public class LoggingConfiguration {
     @Bean
     RequestRejectedHandler requestRejectedHandler() {
         return new HttpStatusRequestRejectedHandler();
+    }
+
+    @Bean
+    public HttpFirewall allowUrlSemicolonHttpFirewall() {
+        StrictHttpFirewall firewall = new StrictHttpFirewall();
+        firewall.setAllowUrlEncodedSlash(true);
+        return firewall;
     }
 
 }
