0e5b75bdcd097c735845dc2d4f2c89620c7cff5e
Tomas Fuentes <tomasfacal@gmail.com>
2021-10-28 12:43:04 +0200

    created preferencesDao

---
+++ appwsc-qsallcomponents-jee-model/src/main/java/com/imatia/qsallcomponents/model/dao/PreferencesDao.java
@@ -0,0 +1,20 @@
+package com.imatia.qsallcomponents.model.dao;
+
+import org.springframework.context.annotation.Lazy;
+import org.springframework.stereotype.Repository;
+
+import com.ontimize.jee.server.dao.common.ConfigurationFile;
+import com.ontimize.jee.server.dao.jdbc.OntimizeJdbcDaoSupport;
+import com.ontimize.jee.server.services.reportstore.dao.IPreferencesDao;
+
+@Lazy
+@Repository(value = "PreferencesDao")
+@ConfigurationFile(configurationFile = "base-dao/PreferencesDao.xml", configurationFilePlaceholder = "base-dao/placeholders.properties")
+public class PreferencesDao extends OntimizeJdbcDaoSupport implements IPreferencesDao {
+
+	public static final String ATTR_ID = "ID";
+	public static final String ATTR_NAME = "NAME";
+	public static final String ATTR_DESCRIPTION = "DESCRIPTION";
+	public static final String ATTR_PREFERENCES = "PREFERENCES";
+
+}
