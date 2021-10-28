55797841153ca2ee1a28223a980bf31d8a804505
Xoán Loureiro Santamaría <xoan.loureiro@imatiasl.lan>
2021-10-28 10:18:54 +0200

    Integrate the multitenant support on Ontimize

--- appwsc-qsallcomponents-jee-model/src/main/java/com/imatia/qsallcomponents/model/dao/tenant/TenantDao.java
+++ appwsc-qsallcomponents-jee-model/src/main/java/com/imatia/qsallcomponents/model/dao/tenant/TenantDao.java
@@ -3,8 +3,8 @@ package com.imatia.qsallcomponents.model.dao.tenant;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Repository;
 
-import com.imatia.qsallcomponents.tenant.store.TenantStoreDao;
 import com.ontimize.jee.server.dao.common.ConfigurationFile;
+import com.ontimize.jee.server.multitenant.jdbc.TenantStoreDao;
 
 @Lazy
 @Repository(value = "TenantDao")
