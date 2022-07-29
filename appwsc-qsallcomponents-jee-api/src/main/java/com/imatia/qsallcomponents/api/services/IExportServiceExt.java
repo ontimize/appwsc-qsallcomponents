b46df48c7e2e8194a614b373e905bf9fef37829d
Daniel Grana <daniel.grana@imatia.com>
2022-07-29 10:48:16 +0200

    Custom CSV exporter

--- appwsc-qsallcomponents-jee-api/src/main/java/com/imatia/qsallcomponents/api/services/IExportServiceExt.java
+++ appwsc-qsallcomponents-jee-api/src/main/java/com/imatia/qsallcomponents/api/services/IExportServiceExt.java
@@ -6,8 +6,8 @@ import java.util.List;
 import com.ontimize.jee.common.dto.EntityResult;
 import com.ontimize.jee.webclient.export.base.IExcelExportService;
 
-public interface IExportServiceExt extends IExcelExportService {
+public interface IExportServiceExt {
 
-	public File csvExport(EntityResult data, List<String> columns) throws Exception;
+	public File csvExport(EntityResult data, List<Object> columns) throws Exception;
 
 }
---
+++ appwsc-qsallcomponents-jee-boot/src/main/java/com/imatia/qsallcomponents/configuration/ExportAutoConfigurationExt.java
@@ -0,0 +1,29 @@
+package com.imatia.qsallcomponents.configuration;
+
+import com.imatia.qsallcomponents.model.service.ExportServiceExt;
+import com.imatia.qsallcomponents.ws.rest.ExportRestControllerExt;
+import com.ontimize.boot.export.OExportAutoConfigure;
+import com.ontimize.jee.webclient.export.base.ExportRestController;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.Primary;
+
+
+@Configuration
+@Import({OExportAutoConfigure.class})
+@ConditionalOnProperty(name = "ontimize.export.enable", havingValue = "true", matchIfMissing = false)
+public class ExportAutoConfigurationExt {
+
+    @Bean("ExportRestController")
+    @Primary
+    public ExportRestController exportRestController() {
+        return new ExportRestControllerExt();
+    }
+
+    @Bean("CsvExportService")
+    public ExportServiceExt exportService() {
+        return new ExportServiceExt();
+    }
+}
