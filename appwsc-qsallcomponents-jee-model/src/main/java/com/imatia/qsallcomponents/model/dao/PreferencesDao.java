656589f781c946a65ae47ccf26415d50bc8fba7e
Tomas Fuentes <tomasfacal@gmail.com>
2022-11-21 13:35:48 +0100

    manage preferences

--- appwsc-qsallcomponents-jee-model/src/main/java/com/imatia/qsallcomponents/model/dao/PreferencesDao.java
+++ appwsc-qsallcomponents-jee-model/src/main/java/com/imatia/qsallcomponents/model/dao/PreferencesDao.java
@@ -1,20 +1,21 @@
 package com.imatia.qsallcomponents.model.dao;
 
-import com.ontimize.jee.report.server.dao.IPreferencesDao;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Repository;
 
 import com.ontimize.jee.server.dao.common.ConfigurationFile;
 import com.ontimize.jee.server.dao.jdbc.OntimizeJdbcDaoSupport;
+import com.ontimize.jee.webclient.preferences.IPreferencesDao;
 
 @Lazy
 @Repository(value = "PreferencesDao")
 @ConfigurationFile(configurationFile = "base-dao/PreferencesDao.xml", configurationFilePlaceholder = "base-dao/placeholders.properties")
 public class PreferencesDao extends OntimizeJdbcDaoSupport implements IPreferencesDao {
 
-	public static final String ATTR_ID = "ID";
-	public static final String ATTR_NAME = "NAME";
-	public static final String ATTR_DESCRIPTION = "DESCRIPTION";
-	public static final String ATTR_PREFERENCES = "PREFERENCES";
+    public static final String ATTR_ID = "ID";
+    public static final String ATTR_NAME = "NAME";
+    public static final String ATTR_DESCRIPTION = "DESCRIPTION";
+    public static final String ATTR_PREFERENCES = "PREFERENCES";
+    public static final String ATTR_TYPE = "TYPE";
 
 }
