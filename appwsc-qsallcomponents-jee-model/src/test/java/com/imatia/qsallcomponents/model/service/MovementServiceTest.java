e16d42c16834ff7c5800fcf12381e040ae3c0a0e
Lucía Fernández Nuñez <lucia.fernandez@imatia.com>
2023-02-02 15:52:35 +0100

    Terminated test class MovementServiceTest

--- appwsc-qsallcomponents-jee-model/src/test/java/com/imatia/qsallcomponents/model/service/MovementServiceTest.java
+++ appwsc-qsallcomponents-jee-model/src/test/java/com/imatia/qsallcomponents/model/service/MovementServiceTest.java
@@ -1,11 +1,13 @@
 package com.imatia.qsallcomponents.model.service;
 
+import com.imatia.qsallcomponents.model.dao.CustomerDao;
 import com.imatia.qsallcomponents.model.dao.MovementDao;
 import com.imatia.qsallcomponents.model.dao.MovementTypeDao;
 import com.ontimize.jee.common.db.AdvancedEntityResult;
 import com.ontimize.jee.common.db.AdvancedEntityResultMapImpl;
 import com.ontimize.jee.common.dto.EntityResult;
 import com.ontimize.jee.common.dto.EntityResultMapImpl;
+import com.ontimize.jee.common.exceptions.OntimizeJEERuntimeException;
 import com.ontimize.jee.server.dao.DefaultOntimizeDaoHelper;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -94,6 +96,117 @@ class MovementServiceTest {
 
 
         }
+
+        @Test
+        void when_movementUpdate_receive_attributes_and_keyValues_and_expected_EntityResult() {
+
+            Map<String, Object> attributes = new HashMap<>();
+            attributes.put("attributes1", "value1");
+            keysValues.put("keysvalues1", "value1");
+
+            EntityResult toRet = new EntityResultMapImpl(EntityResult.OPERATION_SUCCESSFUL, EntityResult.DATA_RESULT);
+            HashMap record = new HashMap<>();
+            record.put("attributes1", "value1");
+            record.put("keysvalues1", "value1");
+            toRet.addRecord(record);
+            Mockito.doReturn(toRet).when(daoHelper).update(movementDao, attributes, keysValues);
+            EntityResult entityResult = movementService.movementUpdate(attributes, keysValues);
+            assertEquals(toRet, entityResult);
+
+        }
+
+        @Test
+        void when_movementDelete_receive_keyValues_expected_EntityResult() {
+            Map<String, Object> keyValues = new HashMap<>();
+            keyValues.put("field1", "value1");
+
+            EntityResult toRet = new EntityResultMapImpl(EntityResult.OPERATION_SUCCESSFUL, EntityResult.DATA_RESULT);
+            HashMap record = new HashMap<>();
+            record.put("field1", "value1");
+            toRet.addRecord(record);
+            Mockito.doReturn(toRet).when(daoHelper).delete(movementDao, keyValues);
+            EntityResult entityResult = movementService.movementDelete(keyValues);
+            assertEquals(toRet, entityResult);
+
+        }
+
+
+    }
+
+
+    @Nested
+    class MovementType {
+
+        Map<String, Object> keysValues = new HashMap<>();
+        List<String> attributes = new ArrayList<>(Arrays.asList("attribute1"));
+
+
+        @Test
+        void when_movementTypeQuery_receive_keysValues_and_attributes_expected_EntityResult() {
+            attributes.add("employeeTypeQuery");
+            keysValues.put("keysvalues1", "value1");
+
+            EntityResult toRet = new EntityResultMapImpl(EntityResult.OPERATION_SUCCESSFUL, EntityResult.DATA_RESULT);
+            HashMap record = new HashMap<>();
+            record.put("attributes1", "value1");
+            record.put("keysvalues1", "value1");
+            toRet.addRecord(record);
+            Mockito.doReturn(toRet).when(daoHelper).query(movementTypeDao, keysValues, attributes);
+            EntityResult entityResult = movementService.movementTypeQuery(keysValues, attributes);
+            assertEquals(toRet, entityResult);
+
+        }
+
+        @Test
+        void when_movementTypeInsert_receive_attributes_expected_EntityResult() {
+            Map<String, Object> attributes = new HashMap<>();
+            attributes.put("field1", "value1");
+
+            EntityResult toRet = new EntityResultMapImpl(EntityResult.OPERATION_SUCCESSFUL, EntityResult.DATA_RESULT);
+            HashMap record = new HashMap<>();
+            record.put("attributes1", "value1");
+            record.put("field1", "value1");
+            toRet.addRecord(record);
+            Mockito.doReturn(toRet).when(daoHelper).insert(movementTypeDao, attributes);
+            EntityResult entityResult = movementService.movementTypeInsert(attributes);
+            assertEquals(toRet, entityResult);
+
+        }
+
+
+        @Test
+        void when_movementTypeUpdate_receive_attributes_and_keyValues_and_expected_EntityResult() {
+
+            Map<String, Object> attributes = new HashMap<>();
+            attributes.put("attributes1", "value1");
+            keysValues.put("keysvalues1", "value1");
+
+            EntityResult toRet = new EntityResultMapImpl(EntityResult.OPERATION_SUCCESSFUL, EntityResult.DATA_RESULT);
+            HashMap record = new HashMap<>();
+            record.put("attributes1", "value1");
+            record.put("keysvalues1", "value1");
+            toRet.addRecord(record);
+            Mockito.doReturn(toRet).when(daoHelper).update(movementTypeDao, attributes, keysValues);
+            EntityResult entityResult = movementService.movementTypeUpdate(attributes, keysValues);
+            assertEquals(toRet, entityResult);
+
+        }
+
+        @Test
+        void when_movementTypeDelete_receive_keyValues_expected_EntityResult() {
+            Map<String, Object> keyValues = new HashMap<>();
+            keyValues.put("field1", "value1");
+
+            EntityResult toRet = new EntityResultMapImpl(EntityResult.OPERATION_SUCCESSFUL, EntityResult.DATA_RESULT);
+            HashMap record = new HashMap<>();
+            record.put("field1", "value1");
+            toRet.addRecord(record);
+            Mockito.doReturn(toRet).when(daoHelper).delete(movementTypeDao, keyValues);
+            EntityResult entityResult = movementService.movementTypeDelete(keyValues);
+            assertEquals(toRet, entityResult);
+
+        }
+
 
 
     }
