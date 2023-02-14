bd64b33e84fb9c949272720b4852a4fde63037a2
Faustino Lage Rego <faustino.lage@gmail.com>
2023-02-14 19:55:59 +0100

    Integration test

--- appwsc-qsallcomponents-jee-boot/src/test/java/com/imatia/qsallcomponents/model/service/EmployeeServiceIT.java
+++ appwsc-qsallcomponents-jee-boot/src/test/java/com/imatia/qsallcomponents/model/service/EmployeeServiceIT.java
@@ -2,15 +2,23 @@ package com.imatia.qsallcomponents.model.service;
 
 import com.imatia.qsallcomponents.api.services.IEmployeeService;
 import com.imatia.qsallcomponents.model.dao.EmployeeDao;
+import com.imatia.qsallcomponents.model.dao.EmployeeTypeDao;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@SpringBootTest(classes={EmployeeService.class, EmployeeDao.class})
+@SpringBootTest(classes={
+        EmployeeService.class,
+        EmployeeDao.class,
+        EmployeeTypeDao.class
+})
 @ExtendWith(SpringExtension.class)
+@EnableAutoConfiguration
 public class EmployeeServiceIT {
 
     @Autowired
