6277600c3efe1b5c4cfac5378475586686ede279
Lucía Fernández Nuñez <lucia.fernandez@imatia.com>
2023-03-24 13:19:31 +0100

    Modify unit test class BranchRestControllerTest

--- appwsc-qsallcomponents-jee-boot/src/test/java/com/imatia/qsallcomponents/ws/rest/BranchRestControllerTest.java
+++ appwsc-qsallcomponents-jee-boot/src/test/java/com/imatia/qsallcomponents/ws/rest/BranchRestControllerTest.java
@@ -10,8 +10,10 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.http.ResponseEntity;
+import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
@@ -46,7 +48,31 @@ class BranchRestControllerTest {
     class Plan {
 
         @Test
-        void when_plan_receive_names_and_files_and_data_expected_ResponseEntity() throws IOException {
+        void when_plan_receive_names_and_files_and_data_expected_ResponseEntity_without_data_without_files() throws IOException {
+            List<String> names = new ArrayList<>();
+            names.add("names");
+
+            List<MultipartFile> files = new ArrayList<>();
+
+            String data =null;
+
+            Map<String, Object> keysValues = new HashMap<>();
+            keysValues.put("string1", "object1");
+
+            EntityResult entityResult = new EntityResultMapImpl(EntityResult.OPERATION_SUCCESSFUL, EntityResult.DATA_RESULT);
+            HashMap record = new HashMap<>();
+            record.put("attributes1", "value1");
+            record.put("field1", "value1");
+            entityResult.addRecord(record);
+
+            ResponseEntity<EntityResult> data1 = branchRestController.plan(names, files, data);
+            assertEquals(1, entityResult.OPERATION_WRONG);
+            assertEquals("<200 OK OK,EntityResult:  ERROR CODE RETURN: ERROR_NO_FILES_INSERTED : {},[]>", data1.toString());
+
+        }
+
+        @Test
+        void when_plan_receive_names_and_files_and_data_expected_ResponseEntity_with_data_without_files() throws IOException {
             List<String> names = new ArrayList<>();
             names.add("names");
 
@@ -65,7 +91,35 @@ class BranchRestControllerTest {
 
             ResponseEntity<EntityResult> data1 = branchRestController.plan(names, files, data);
             assertNotNull(data1);
-            assertEquals("ERROR_NO_FILES_INSERTED", entityResult.OPERATION_WRONG);
+            assertEquals(1, entityResult.OPERATION_WRONG);
+            assertEquals("<200 OK OK,EntityResult:  ERROR CODE RETURN: ERROR_NO_FILES_INSERTED : {},[]>", data1.toString());
+
+        }
+
+        @Test
+        void when_plan_receive_names_and_files_and_data_expected_ResponseEntity_with_data_with_files() throws IOException {
+            List<String> names = new ArrayList<>();
+            names.add("names");
+
+            MultipartFile file = Mockito.mock(MultipartFile.class);
+            List<MultipartFile> files = new ArrayList<>();
+            files.add(file);
+
+            String data ="{\"string1\":\"object1\"}";
+
+            Map<String, Object> keysValues = new HashMap<>();
+            keysValues.put("string1", "object1");
+
+            EntityResult entityResult = new EntityResultMapImpl(EntityResult.OPERATION_SUCCESSFUL, EntityResult.DATA_RESULT);
+            HashMap record = new HashMap<>();
+            record.put("attributes1", "value1");
+            record.put("field1", "value1");
+            entityResult.addRecord(record);
+
+            ResponseEntity<EntityResult> data1 = branchRestController.plan(names, files, data);
+            assertNotNull(data1);
+            assertEquals(1, entityResult.OPERATION_WRONG);
+            assertEquals("<200 OK OK,[]>", data1.toString());
 
         }
     }
